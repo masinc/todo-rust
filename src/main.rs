6cc0cc0c54a5000ceecdf296cd5ae11a67957e5b
masinc <masinc000@gmail.com>
2020-09-03 20:50:10 +0900

    add askama

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,23 @@
 use actix_web::{get, App, HttpResponse, HttpServer};
+use askama::Template;
 use thiserror::Error;
 
+struct TodoEntry {
+    id: usize,
+    text: String,
+}
+
+#[derive(Template)]
+#[template(path = "index.html")]
+struct IndexTemplate {
+    entries: Vec<TodoEntry>,
+}
+
 #[derive(Error, Debug)]
-enum AppError {}
+enum AppError {
+    #[error("Failed to render HTML")]
+    AskamaError(#[from] askama::Error),
+}
 
 impl actix_web::ResponseError for AppError {}
 
@@ -10,13 +25,26 @@ type AppResult<T> = Result<T, AppError>;
 
 #[get("/")]
 async fn index() -> AppResult<HttpResponse> {
-    let response_body = "Hello World";
+    let mut entries = vec![];
+    entries.push(TodoEntry {
+        id: 1,
+        text: "First entry".to_string(),
+    });
 
-    Ok(HttpResponse::Ok().body(response_body))
+    entries.push(TodoEntry {
+        id: 2,
+        text: "Second entry".to_string(),
+    });
+
+    let html = IndexTemplate { entries };
+    let response_body = html.render()?;
+    Ok(HttpResponse::Ok()
+        .content_type("text/html")
+        .body(response_body))
 }
 
 #[actix_rt::main]
-async fn main() -> AppResult<()> {
+async fn main() -> Result<(), actix_web::Error> {
     HttpServer::new(move || App::new().service(index))
         .bind("0.0.0.0:8080")?
         .run()
