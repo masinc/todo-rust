01ce568072f37ae88025ef208d70c8c9ac305c5b
masinc <masinc000@gmail.com>
2020-09-03 20:17:41 +0900

    add web hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpResponse, HttpServer};
+type ActixResult<T> = Result<T, actix_web::Error>;
+
+#[get("/")]
+async fn index() -> ActixResult<HttpResponse> {
+    let response_body = "Hello World";
+
+    Ok(HttpResponse::Ok().body(response_body))
+}
+
+#[actix_rt::main]
+async fn main() -> ActixResult<()> {
+    HttpServer::new(move || App::new().service(index))
+        .bind("0.0.0.0:8080")?
+        .run()
+        .await?;
+
+    Ok(())
 }
